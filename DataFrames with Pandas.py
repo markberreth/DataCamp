e34cb78b5543f8583ebb7c1703b55bf3efde751b
Mark Berreth <mark.berreth@gmail.com>
2018-08-27 14:17:50 -0700

    Started new course of Pandas DataFrame operations.

---
+++ DataFrames with Pandas.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# Assign the row position of election.loc['Bedford']: x
+x = 4
+
+# Assign the column position of election['winner']: y
+y = 4
+
+# Print the boolean equivalence
+print(election.iloc[x, y] == election.loc['Bedford', 'winner'])
+
+# Read in filename and set the index: election
+election = pd.read_csv(filename, index_col='county')
+
+# Create a separate dataframe with the columns ['winner', 'total', 'voters']: results
+results = election[['winner', 'total', 'voters']]
+
+# Print the output of results.head()
+print(results.head())
