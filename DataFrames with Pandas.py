33025f678a2aab1f4dac3d30f05fc5708991aee0
Mark Berreth <mark.berreth@gmail.com>
2018-08-30 16:06:57 -0700

    Finished course.

--- DataFrames with Pandas.py
+++ DataFrames with Pandas.py
@@ -285,3 +285,303 @@ by_city_day = users.pivot_table(index=['weekday'],columns=['city'])
 
 # Print by_city_day
 print(by_city_day)
+
+# Use a pivot table to display the count of each column: count_by_weekday1
+count_by_weekday1 = users.pivot_table(aggfunc='count', index='weekday')
+
+# Print count_by_weekday
+print(count_by_weekday1)
+
+# Replace 'aggfunc='count'' with 'aggfunc=len': count_by_weekday2
+count_by_weekday2 = users.pivot_table(aggfunc=len, index='weekday')
+
+# Verify that the same result is obtained
+print('==========================================')
+print(count_by_weekday1.equals(count_by_weekday2))
+
+# Create the DataFrame with the appropriate pivot table: signups_and_visitors
+signups_and_visitors = users.pivot_table(aggfunc=sum,index='weekday')
+
+# Print signups_and_visitors
+print(signups_and_visitors)
+
+# Add in the margins: signups_and_visitors_total
+signups_and_visitors_total = users.pivot_table(aggfunc=sum,index='weekday', margins=True)
+
+# Print signups_and_visitors_total
+print(signups_and_visitors_total)
+
+# Group titanic by 'pclass'
+by_class = titanic.groupby('pclass')
+
+# Aggregate 'survived' column of by_class by count
+count_by_class = by_class['survived'].count()
+
+# Print count_by_class
+print(count_by_class)
+
+# Group titanic by 'embarked' and 'pclass'
+by_mult = titanic.groupby(['embarked','pclass'])
+
+# Aggregate 'survived' column of by_mult by count
+count_mult = by_mult['survived'].count()
+
+# Print count_mult
+print(count_mult)
+
+# Read life_fname into a DataFrame: life
+life = pd.read_csv(life_fname, index_col='Country')
+
+# Read regions_fname into a DataFrame: regions
+regions = pd.read_csv(regions_fname, index_col='Country')
+
+# Group life by regions['region']: life_by_region
+life_by_region = life.groupby(regions['region'])
+
+# Print the mean over the '2010' column of life_by_region
+print(life_by_region['2010'].mean())
+
+# Group titanic by 'pclass': by_class
+by_class = titanic.groupby('pclass')
+
+# Select 'age' and 'fare'
+by_class_sub = by_class[['age','fare']]
+
+# Aggregate by_class_sub by 'max' and 'median': aggregated
+aggregated = by_class_sub.agg(['max','median'])
+
+# Print the maximum age in each class
+print(aggregated.loc[:, ('age','max')])
+
+# Print the median fare in each class
+print(aggregated.loc[:, ('fare','median')])
+
+# Read the CSV file into a DataFrame and sort the index: gapminder
+gapminder = pd.read_csv('gapminder.csv', index_col=['Year','region','Country']).sort_index()
+
+# Group gapminder by 'Year' and 'region': by_year_region
+by_year_region = gapminder.groupby(['Year','region'])
+
+# Define the function to compute spread: spread
+def spread(series):
+    return series.max() - series.min()
+
+# Create the dictionary: aggregator
+aggregator = {'population':'sum', 'child_mortality':'mean', 'gdp':spread}
+
+# Aggregate by_year_region using the dictionary: aggregated
+aggregated = by_year_region.agg(aggregator)
+
+# Print the last 6 entries of aggregated
+print(aggregated.tail(6))
+
+# Read file: sales
+sales = pd.read_csv('sales.csv', index_col='Date', parse_dates=True)
+
+# Create a groupby object: by_day
+by_day = sales.groupby(sales.index.strftime('%a'))
+
+# Create sum: units_sum
+units_sum = by_day['Units'].sum()
+
+# Print units_sum
+print(units_sum)
+
+# Import zscore
+from scipy.stats import zscore
+
+# Group gapminder_2010: standardized
+standardized = gapminder_2010.groupby('region')[['life','fertility']].transform(zscore)
+
+# Construct a Boolean Series to identify outliers: outliers
+outliers = (standardized['life'] < -3) | (standardized['fertility'] > 3)
+
+# Filter gapminder_2010 by the outliers: gm_outliers
+gm_outliers = gapminder_2010.loc[outliers]
+
+# Print gm_outliers
+print(gm_outliers)
+
+# Create a groupby object: by_sex_class
+by_sex_class = titanic.groupby(['sex','pclass'])
+
+# Write a function that imputes median
+def impute_median(series):
+    return series.fillna(series.median())
+
+# Impute age and assign to titanic['age']
+titanic.age = by_sex_class['age'].transform(impute_median)
+
+# Print the output of titanic.tail(10)
+print(titanic.tail(10))
+
+# Group gapminder_2010 by 'region': regional
+regional = gapminder_2010.groupby('region')
+
+# Apply the disparity function on regional: reg_disp
+reg_disp = regional.apply(disparity)
+
+# Print the disparity of 'United States', 'United Kingdom', and 'China'
+print(reg_disp.loc[['United States','United Kingdom','China']])
+
+# Create a groupby object using titanic over the 'sex' column: by_sex
+by_sex = titanic.groupby('sex')
+
+# Call by_sex.apply with the function c_deck_survival
+c_surv_by_sex = by_sex.apply(c_deck_survival)
+
+# Print the survival rates
+print(c_surv_by_sex)
+
+# Read the CSV file into a DataFrame: sales
+sales = pd.read_csv('sales.csv', index_col='Date', parse_dates=True)
+
+# Group sales by 'Company': by_company
+by_company = sales.groupby('Company')
+
+# Compute the sum of the 'Units' of by_company: by_com_sum
+by_com_sum = by_company['Units'].sum()
+print(by_com_sum)
+
+# Filter 'Units' where the sum is > 35: by_com_filt
+by_com_filt = by_company.filter(lambda g:g['Units'].sum() > 35)
+print(by_com_filt)
+
+# Create the Boolean Series: under10
+under10 = (titanic['age'] < 10).map({True:'under 10',False:'over 10'})
+
+# Group by under10 and compute the survival rate
+survived_mean_1 = titanic['survived'].groupby(under10).mean()
+print(survived_mean_1)
+
+# Group by under10 and pclass and compute the survival rate
+survived_mean_2 = titanic['survived'].groupby([under10,titanic['pclass']]).mean()
+print(survived_mean_2)
+
+# Select the 'NOC' column of medals: country_names
+country_names = medals['NOC']
+
+# Count the number of medals won by each country: medal_counts
+medal_counts = country_names.value_counts()
+
+# Print top 15 countries ranked by medals
+print(medal_counts.head(15))
+
+# Construct the pivot table: counted
+counted = medals.pivot_table(index="NOC",values='Athlete',columns='Medal',aggfunc='count')
+
+# Create the new column: counted['totals']
+counted['totals'] = counted.sum(axis='columns')
+
+# Sort counted by the 'totals' column
+counted = counted.sort_values('totals',ascending=False)
+
+# Print the top 15 rows of counted
+print(counted.head(15))
+
+# Select columns: ev_gen
+ev_gen = medals[['Event_gender','Gender']]
+
+# Drop duplicate pairs: ev_gen_uniques
+ev_gen_uniques = ev_gen.drop_duplicates()
+
+# Print ev_gen_uniques
+print(ev_gen_uniques)
+
+# Group medals by the two columns: medals_by_gender
+medals_by_gender = medals.groupby(['Event_gender','Gender'])
+
+# Create a DataFrame with a group count: medal_count_by_gender
+medal_count_by_gender = medals_by_gender.count()
+
+# Print medal_count_by_gender
+print(medal_count_by_gender)
+
+# Create the Boolean Series: sus
+sus = (medals.Event_gender == 'W') & (medals.Gender == 'Men')
+
+# Create a DataFrame with the suspicious row: suspect
+suspect = medals[sus]
+
+# Print suspect
+print(suspect)
+
+# Group medals by 'NOC': country_grouped
+country_grouped = medals.groupby('NOC')
+
+# Compute the number of distinct sports in which each country won medals: Nsports
+Nsports = country_grouped['Sport'].nunique()
+
+# Sort the values of Nsports in descending order
+Nsports = Nsports.sort_values(ascending=False)
+
+# Print the top 15 rows of Nsports
+print(Nsports.head(15))
+
+# Extract all rows for which the 'Edition' is between 1952 & 1988: during_cold_war
+during_cold_war = (medals['Edition'] >= 1952) & (medals['Edition'] <= 1988)
+
+# Extract rows for which 'NOC' is either 'USA' or 'URS': is_usa_urs
+is_usa_urs = medals.NOC.isin(['USA','URS'])
+
+# Use during_cold_war and is_usa_urs to create the DataFrame: cold_war_medals
+cold_war_medals = medals.loc[during_cold_war & is_usa_urs]
+
+# Group cold_war_medals by 'NOC'
+country_grouped = cold_war_medals.groupby('NOC')
+
+# Create Nsports
+Nsports = country_grouped['Sport'].nunique().sort_values(ascending=False)
+
+# Print Nsports
+print(Nsports)
+
+# Create the pivot table: medals_won_by_country
+medals_won_by_country = medals.pivot_table(index='Edition',columns='NOC',values='Athlete',aggfunc='count')
+
+# Slice medals_won_by_country: cold_war_usa_urs_medals
+cold_war_usa_urs_medals = medals_won_by_country.loc[1952:1988, ['USA','URS']]
+
+# Create most_medals
+most_medals = cold_war_usa_urs_medals.idxmax(axis='columns')
+
+# Print most_medals.value_counts()
+print(most_medals.value_counts())
+
+# Create the DataFrame: usa
+usa = medals[medals.NOC == 'USA']
+
+# Group usa by ['Edition', 'Medal'] and aggregate over 'Athlete'
+usa_medals_by_year = usa.groupby(['Edition', 'Medal'])['Athlete'].count()
+
+# Reshape usa_medals_by_year by unstacking
+usa_medals_by_year = usa_medals_by_year.unstack(level='Medal')
+
+# Create the DataFrame: usa
+usa = medals[medals.NOC == 'USA']
+
+# Group usa by 'Edition', 'Medal', and 'Athlete'
+usa_medals_by_year = usa.groupby(['Edition', 'Medal'])['Athlete'].count()
+
+# Reshape usa_medals_by_year by unstacking
+usa_medals_by_year = usa_medals_by_year.unstack(level='Medal')
+
+# Create an area plot of usa_medals_by_year
+usa_medals_by_year.plot.area()
+plt.show()
+
+# Redefine 'Medal' as an ordered categorical
+medals.Medal = pd.Categorical(values=medals.Medal,categories=['Bronze','Silver','Gold'],ordered=True)
+medals.info()
+# Create the DataFrame: usa
+usa = medals[medals.NOC == 'USA']
+
+# Group usa by 'Edition', 'Medal', and 'Athlete'
+usa_medals_by_year = usa.groupby(['Edition', 'Medal'])['Athlete'].count()
+
+# Reshape usa_medals_by_year by unstacking
+usa_medals_by_year = usa_medals_by_year.unstack(level='Medal')
+
+# Create an area plot of usa_medals_by_year
+usa_medals_by_year.plot.area()
+plt.show()
