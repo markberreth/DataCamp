2589478ffb4d609da0b6969a2fc9c4b32cb20855
Mark Berreth <mark.berreth@gmail.com>
2018-08-31 13:25:27 -0700

    Starting new course. Merging DataFrames.

--- Merging DataFrames with pandas.py
+++ Merging DataFrames with pandas.py
@@ -13,3 +13,95 @@ gold = pd.read_csv('Gold.csv')
 # Print the first five rows of gold
 print(gold.head())
 
+# Import pandas
+import pandas as pd
+
+# Create the list of file names: filenames
+filenames = ['Gold.csv', 'Silver.csv', 'Bronze.csv']
+
+# Create the list of three DataFrames: dataframes
+dataframes = []
+for filename in filenames:
+    dataframes.append(pd.read_csv(filename))
+
+# Print top 5 rows of 1st DataFrame in dataframes
+print(dataframes[0].head())
+
+# Make a copy of gold: medals
+medals = gold.copy()
+
+# Create list of new column labels: new_labels
+new_labels = ['NOC', 'Country', 'Gold']
+
+# Rename the columns of medals using new_labels
+medals.columns = new_labels
+
+# Add columns 'Silver' & 'Bronze' to medals
+medals['Silver'] = silver.Total
+medals['Bronze'] = bronze.Total
+
+# Print the head of medals
+print(medals.head())
+
+# Import pandas
+import pandas as pd
+
+# Read 'monthly_max_temp.csv' into a DataFrame: weather1
+weather1 = pd.read_csv('monthly_max_temp.csv', index_col='Month')
+
+# Print the head of weather1
+print(weather1.head())
+
+# Sort the index of weather1 in alphabetical order: weather2
+weather2 = weather1.sort_index(ascending=True)
+
+# Print the head of weather2
+print(weather2.head())
+
+# Sort the index of weather1 in reverse alphabetical order: weather3
+weather3 = weather1.sort_index(ascending=False)
+
+# Print the head of weather3
+print(weather3.head())
+
+# Sort weather1 numerically using the values of 'Max TemperatureF': weather4
+weather4 = weather1.sort_values('Max TemperatureF')
+
+# Print the head of weather4
+print(weather4.head())
+
+# Reindex weather1 using the list year: weather2
+weather2 = weather1.reindex(year)
+
+# Print weather2
+print(weather2)
+
+# Reindex weather1 using the list year with forward-fill: weather3
+weather3 = weather1.reindex(year).ffill()
+
+# Print weather3
+print(weather3)
+
+# Reindex names_1981 with index of names_1881: common_names
+common_names = names_1981.reindex(index=names_1881.index)
+
+# Print shape of common_names
+print(common_names.shape)
+
+# Drop rows with null counts: common_names
+common_names = common_names.dropna()
+
+# Print shape of new common_names
+print(common_names.shape)
+
+# Extract selected columns from weather as new DataFrame: temps_f
+temps_f = weather[['Min TemperatureF','Mean TemperatureF','Max TemperatureF']]
+
+# Convert temps_f to celsius: temps_c
+temps_c = ((temps_f - 32) * 5/9)
+
+# Rename 'F' in column names with 'C': temps_c.columns
+temps_c.columns = temps_c.columns.str.replace('F','C')
+
+# Print first 5 rows of temps_c
+print(temps_c.head())
