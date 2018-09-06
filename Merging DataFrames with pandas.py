5293cd10168633e1170fe62692cb33d8b2e65341
Mark Berreth <mark.berreth@gmail.com>
2018-09-06 10:27:31 -0700

    Worked most of today on the course. Made significant headway.

--- Merging DataFrames with pandas.py
+++ Merging DataFrames with pandas.py
@@ -459,3 +459,10 @@ medals = pd.concat(medals_dict, ignore_index=True)
 # Print first and last 5 rows of medals
 print(medals.head())
 print(medals.tail())
+
+# Construct the pivot_table: medal_counts
+medal_counts = medals.pivot_table(aggfunc='count',index='Edition',values='Athlete',columns='NOC')
+
+# Print the first & last 5 rows of medal_counts
+print(medal_counts.head())
+print(medal_counts.tail())
