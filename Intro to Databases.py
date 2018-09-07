f727cd24d8543b053d1ecf654ce2f5d866794041
Mark Berreth <mark.berreth@gmail.com>
2018-09-07 10:58:49 -0700

    Not quite through first chapter. Struggling to focus today.

--- Intro to Databases.py
+++ Intro to Databases.py
@@ -7,3 +7,45 @@ engine = create_engine('sqlite:///census.sqlite')
 
 # Print table names
 print(engine.table_names())
+
+# Import Table
+from sqlalchemy import Table
+
+# Reflect census table from the engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Print census table metadata
+print(repr(census))
+
+# Reflect the census table from the engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Print the column names
+print(census.columns.keys())
+
+# Print full table metadata
+print(repr(metadata.tables['census']))
+
+# Build select statement for census table: stmt
+stmt = 'select * from census'
+
+# Execute the statement and fetch the results: results
+results = connection.execute(stmt).fetchall()
+
+# Print results
+print(results)
+
+# Import select
+from sqlalchemy import select
+
+# Reflect census table via engine: census
+census = Table('census', metadata, autoload=True, autoload_with=engine)
+
+# Build select statement for census table: stmt
+stmt = select([census])
+
+# Print the emitted statement to see the SQL emitted
+print(stmt)
+
+# Execute the statement and print the results
+print(connection.execute(stmt).fetchall())
