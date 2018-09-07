068681dca0430d3017f1691ed39a66e04591771a
Mark Berreth <mark.berreth@gmail.com>
2018-09-07 08:56:32 -0700

    Starting Database course with SQLAlchemy.

---
+++ Intro to Databases.py
@@ -0,0 +1,9 @@
+# New course started 2018/9/7
+# Import create_engine
+from sqlalchemy import create_engine
+
+# Create an engine that connects to the census.sqlite file: engine
+engine = create_engine('sqlite:///census.sqlite')
+
+# Print table names
+print(engine.table_names())
