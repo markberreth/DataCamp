64c3e321638829ed811be420a1c9a870ac14e18e
Mark Berreth <mark.berreth@gmail.com>
2018-09-10 16:32:56 -0700

    Finished first chapter. Made it through part of second chapter.

--- Intro to Databases.py
+++ Intro to Databases.py
@@ -138,3 +138,15 @@ rev_results = connection.execute(rev_stmt).fetchall()
 
 # Print the first 10 rev_results
 print(rev_results[:10])
+
+# Build a query to select state and age: stmt
+stmt = select([census.columns.state, census.columns.age])
+
+# Append order by to ascend by state and descend by age
+stmt = stmt.order_by((census.columns.state), desc(census.columns.age))
+
+# Execute the statement and store all the records: results
+results = connection.execute(stmt).fetchall()
+
+# Print the first 20 results
+print(results[:20])
