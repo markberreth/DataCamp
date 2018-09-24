8e367e58d8dda3c70f882d2d01f4f69e1fdd0ca1
Mark Berreth <mark.berreth@gmail.com>
2018-09-24 14:44:03 -0700

    Finished first chapter. Made it through part of second chapter.

--- Intro to Databases.py
+++ Intro to Databases.py
@@ -150,3 +150,30 @@ results = connection.execute(stmt).fetchall()
 
 # Print the first 20 results
 print(results[:20])
+
+# Build a query to count the distinct states values: stmt
+stmt = select([func.count(census.columns.state.distinct())])
+
+# Execute the query and store the scalar result: distinct_state_count
+distinct_state_count = connection.execute(stmt).scalar()
+
+# Print the distinct_state_count
+print(distinct_state_count)
+
+# Import func
+from sqlalchemy import func
+
+# Build a query to select the state and count of ages by state: stmt
+stmt = select([census.columns.state, func.count(census.columns.age)])
+
+# Group stmt by state
+stmt = stmt.group_by(census.columns.state)
+
+# Execute the statement and store all the records: results
+results = connection.execute(stmt).fetchall()
+
+# Print results
+print(results)
+
+# Print the keys/column names of the results returned
+print(results[0].keys())
