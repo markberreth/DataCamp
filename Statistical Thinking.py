9a30856a71493ad445dbba41a579a289844b4746
Mark Berreth <mark.berreth@gmail.com>
2018-10-25 13:57:11 -0700

    Started course. On Chapter 1.

---
+++ Statistical Thinking.py
@@ -0,0 +1,53 @@
+# Import plotting modules
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Set default Seaborn style
+sns.set()
+
+# Plot histogram of versicolor petal lengths
+_ = plt.hist(versicolor_petal_length)
+
+# Show histogram
+plt.show()
+
+# Plot histogram of versicolor petal lengths
+_ = plt.hist(versicolor_petal_length)
+
+# Label axes
+plt.xlabel('petal length (cm)')
+plt.ylabel('count')
+
+# Show histogram
+plt.show()
+
+# Import numpy
+import numpy as np
+
+# Compute number of data points: n_data
+n_data = len(versicolor_petal_length)
+
+# Number of bins is the square root of number of data points: n_bins
+n_bins = np.sqrt(n_data)
+
+# Convert number of bins to integer: n_bins
+n_bins = int(n_bins)
+
+# Plot the histogram
+_ = plt.hist(versicolor_petal_length, bins = n_bins)
+
+# Label axes
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('count')
+
+# Show histogram
+plt.show()
+
+# Create bee swarm plot with Seaborn's default settings
+_ = sns.swarmplot(x='species',y='petal length (cm)',data=df)
+
+# Label the axes
+_ = plt.xlabel('species')
+_ = plt.ylabel('petal length (cm)')
+# Show the plot
+plt.show()
