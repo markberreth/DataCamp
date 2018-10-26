eebe50d13a0be21a48e3c172ec7b38d7a3550119
Mark Berreth <mark.berreth@gmail.com>
2018-10-26 11:47:12 -0700

    Finished Chapter 1.

--- Statistical Thinking.py
+++ Statistical Thinking.py
@@ -51,3 +51,145 @@ _ = plt.xlabel('species')
 _ = plt.ylabel('petal length (cm)')
 # Show the plot
 plt.show()
+
+def ecdf(data):
+    """Compute ECDF for a one-dimensional array of measurements."""
+    # Number of data points: n
+	n = len(data)
+
+    # x-data for the ECDF: x
+	x = np.sort(data)
+
+    # y-data for the ECDF: y
+    y = np.arange(1, n+1) / n
+
+    return x, y
+
+# Compute ECDF for versicolor data: x_vers, y_vers
+x_vers, y_vers = ecdf(versicolor_petal_length)
+
+# Generate plot
+plt.plot(x_vers, y_vers, marker = '.', linestyle='none')
+
+# Label the axes
+plt.xlabel('versicolor petal length (cm)')
+plt.ylabel('ECDF')
+
+# Display the plot
+plt.show()
+
+# Compute ECDFs
+x_set, y_set = ecdf(setosa_petal_length)
+x_vers, y_vers = ecdf(versicolor_petal_length)
+x_virg, y_virg = ecdf(virginica_petal_length)
+
+# Plot all ECDFs on the same plot
+_ = plt.plot(x_set, y_set, marker = '.', linestyle = 'none')
+_ = plt.plot(x_vers, y_vers, marker = '.', linestyle = 'none')
+_ = plt.plot(x_virg, y_virg, marker = '.', linestyle = 'none')
+
+# Annotate the plot
+plt.legend(('setosa', 'versicolor', 'virginica'), loc='lower right')
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('ECDF')
+
+# Display the plot
+plt.show()
+
+# Compute the mean: mean_length_vers
+mean_length_vers = np.mean(versicolor_petal_length)
+
+# Print the result with some nice formatting
+print('I. versicolor:', mean_length_vers, 'cm')
+
+# Specify array of percentiles: percentiles
+percentiles = np.array([2.5, 25, 50, 75, 97.5])
+
+# Compute percentiles: ptiles_vers
+ptiles_vers = np.percentile(versicolor_petal_length, percentiles)
+
+# Print the result
+print(ptiles_vers)
+
+# Plot the ECDF
+_ = plt.plot(x_vers, y_vers, '.')
+_ = plt.xlabel('petal length (cm)')
+_ = plt.ylabel('ECDF')
+
+# Overlay percentiles as red diamonds.
+_ = plt.plot(ptiles_vers, percentiles/100, marker='D', color='red',
+         linestyle='none')
+
+# Show the plot
+plt.show()
+
+# Create box plot with Seaborn's default settings
+_ = sns.boxplot(x = 'species', y = 'petal length (cm)', data = df)
+
+# Label the axes
+plt.xlabel('species')
+plt.ylabel('petal length (cm)')
+
+# Show the plot
+plt.show()
+
+# Array of differences to mean: differences
+differences = versicolor_petal_length - np.mean(versicolor_petal_length)
+
+# Square the differences: diff_sq
+diff_sq = differences**2
+
+# Compute the mean square difference: variance_explicit
+variance_explicit = np.mean(diff_sq)
+
+# Compute the variance using NumPy: variance_np
+variance_np = np.var(versicolor_petal_length)
+
+# Print the results
+print(variance_explicit, variance_np)
+
+# Compute the variance: variance
+variance = np.var(versicolor_petal_length)
+
+# Print the square root of the variance
+print(np.sqrt(variance))
+
+# Print the standard deviation
+print(np.std(versicolor_petal_length))
+
+# Make a scatter plot
+plt.plot(versicolor_petal_length, versicolor_petal_width, marker='.', linestyle='none')
+
+
+# Label the axes
+plt.xlabel('versicolor petal length')
+plt.ylabel('versicolor petal width')
+
+# Show the result
+plt.show()
+
+# Compute the covariance matrix: covariance_matrix
+covariance_matrix = np.cov(versicolor_petal_length, versicolor_petal_width)
+
+# Print covariance matrix
+print(covariance_matrix)
+
+# Extract covariance of length and width of petals: petal_cov
+petal_cov = covariance_matrix[0,1]
+
+# Print the length/width covariance
+print(petal_cov)
+
+def pearson_r(x, y):
+    """Compute Pearson correlation coefficient between two arrays."""
+    # Compute correlation matrix: corr_mat
+    corr_mat = np.corrcoef(x,y)
+
+    # Return entry [0,1]
+    return corr_mat[0,1]
+
+# Compute Pearson correlation coefficient for I. versicolor: r
+r = np.corrcoef(versicolor_petal_length, versicolor_petal_width)[0,1]
+
+# Print the result
+print(r, pearson_r)
