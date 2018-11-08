e49db1742f5aa72f9805d6cdefe2d7d128ce28a3
Mark Berreth <mark.berreth@gmail.com>
2018-11-08 15:30:23 -0800

    Part 2. Chapter 4. Leaving for the day.

--- Statistical Thinking.py
+++ Statistical Thinking.py
@@ -866,3 +866,22 @@ for i in range(10000):
 # Compute p-value: p
 p = np.sum(perm_replicates >= r_obs) / len(perm_replicates)
 print('p-val =', p)
+
+# Compute x,y values for ECDFs
+x_control, y_control = ecdf(control)
+x_treated, y_treated = ecdf(treated)
+
+# Plot the ECDFs
+plt.plot(x_control, y_control, marker='.', linestyle='none')
+plt.plot(x_treated, y_treated, marker='.', linestyle='none')
+
+# Set the margins
+plt.margins(0.02)
+
+# Add a legend
+plt.legend(('control', 'treated'), loc='lower right')
+
+# Label axes and show plot
+plt.xlabel('millions of alive sperm per mL')
+plt.ylabel('ECDF')
+plt.show()
