6073ba1577bfbcaae4e6281ccf6e154b5fd3ace7
Mark Berreth <mark.berreth@gmail.com>
2018-11-16 16:08:03 -0800

    starting new course.

---
+++ Scikit-learn.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+plt.style.use('ggplot')
+
+from sklearn import datasets
+
+iris = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+print(iris.head())
+print(iris.describe())
+
+
+
+
+
+
+
+'''practice starts here'''
+
+# Import KNeighborsClassifier from sklearn.neighbors
+from sklearn.neighbors import KNeighborsClassifier
+
+# Create arrays for the features and the response variable
+y = df['party'].values
+X = df.drop('party', axis=1).values
+
+# Create a k-NN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors = 6)
+
+# Fit the classifier to the data
+knn.fit(X, y)
+
+# Import necessary modules
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+# Load the digits dataset: digits
+digits = datasets.load_digits()
+
+# Print the keys and DESCR of the dataset
+print(digits.keys())
+print(digits.DESCR)
+
+# Print the shape of the images and data keys
+print(digits.images.shape)
+print(digits.data.shape)
+
+# Display digit 1010
+plt.imshow(digits.images[1010], cmap=plt.cm.gray_r, interpolation='nearest')
+plt.show()
