e49b1f472f1d9b5cb927b333fe160b378cb6df54
Mark Berreth <mark.berreth@gmail.com>
2018-12-06 12:10:25 -0800

    Starting new course. Going to lunch.

---
+++ Machine Learning: School Budgets.py
@@ -0,0 +1,48 @@
+#This is a practical of an actual Kaggle project regarding school budget classifications
+
+import pandas as pd
+df = pd.read_csv('TrainingData.csv', index_col=0)
+print(df.info())
+print(df.head(5))
+
+# Print the summary statistics
+print(df.describe())
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Create the histogram
+plt.hist(df['FTE'].dropna())
+
+# Add title and labels
+plt.title('Distribution of %full-time \n employee works')
+plt.xlabel('% of full-time')
+plt.ylabel('num employees')
+
+# Display the histogram
+plt.show()
+
+# Define the lambda function: categorize_label
+categorize_label = lambda x: x.astype('category')
+
+# Convert df[LABELS] to a categorical type
+df[LABELS] = df[LABELS].apply(categorize_label, axis=0)
+
+# Print the converted dtypes
+print(df[LABELS].dtypes)
+
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Calculate number of unique values for each label: num_unique_labels
+num_unique_labels = df[LABELS].apply(pd.Series.nunique)
+
+# Plot number of unique values for each label
+num_unique_labels.plot(kind='bar')
+
+# Label the axes
+plt.xlabel('Labels')
+plt.ylabel('Number of unique values')
+
+# Display the plot
+plt.show()
