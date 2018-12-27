513418a5a341eef45426d831d3b8b0a5cf863f0a
Mark Berreth <mark.berreth@gmail.com>
2018-12-27 15:54:28 -0800

    Leaving for the day. Still on Chapter 1.

--- Manipulating Time Series Data.py
+++ Manipulating Time Series Data.py
@@ -25,3 +25,48 @@ print(data.info())
 # Plot data
 data.plot(subplots=True)
 plt.show()
+
+# Create dataframe prices here
+prices = pd.DataFrame([])
+
+# Select data for each year and concatenate with prices here
+for year in ['2013', '2014', '2015']:
+    price_per_year = yahoo.loc[year, ['price']].reset_index(drop=True)
+    price_per_year.rename(columns={'price': year}, inplace=True)
+    prices = pd.concat([prices, price_per_year], axis=1)
+
+# Plot prices
+prices.plot()
+plt.show()
+
+# Inspect data
+print(co.info())
+
+# Set the frequency to calendar daily
+co = co.asfreq('D')
+
+# Plot the data
+co.plot(subplots=True)
+plt.show()
+
+
+# Set frequency to monthly
+co = co.asfreq('M')
+
+# Plot the data
+co.plot(subplots=True)
+plt.show()
+
+# Import data here
+google = pd.read_csv('google.csv', parse_dates=['Date'], index_col='Date')
+
+# Set data frequency to business daily
+google = google.asfreq('B')
+
+# Create 'lagged' and 'shifted'
+google['lagged'] = google['Close'].shift(periods=-90)
+google['shifted'] = google['Close'].shift(periods=90)
+
+# Plot the google price series
+google.plot(subplots=True)
+plt.show()
