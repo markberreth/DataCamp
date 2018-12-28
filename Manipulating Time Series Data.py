cde79ed628e17a11f4a2a2cafc23e2db7f3c852d
Mark Berreth <mark.berreth@gmail.com>
2018-12-28 15:49:01 -0800

    Leaving for the day.

--- Manipulating Time Series Data.py
+++ Manipulating Time Series Data.py
@@ -70,3 +70,114 @@ google['shifted'] = google['Close'].shift(periods=90)
 # Plot the google price series
 google.plot(subplots=True)
 plt.show()
+
+# Created shifted_30 here
+yahoo['shifted_30'] = yahoo['price'].shift(periods=30)
+
+# Subtract shifted_30 from price
+yahoo['change_30'] = yahoo['price'].sub(yahoo['shifted_30'])
+
+# Get the 30-day price difference
+yahoo['diff_30'] = yahoo['price'].diff(periods=30)
+
+# Inspect the last five rows of price
+print(yahoo['price'].tail(5))
+
+# Show the value_counts of the difference between change_30 and diff_30
+print(yahoo['diff_30'].sub(yahoo['change_30']).value_counts())
+
+# Create daily_return
+google['daily_return'] = google['Close'].pct_change(periods=1) * 100
+
+# Create monthly_return
+google['monthly_return'] = google['Close'].pct_change(periods=30) * 100
+
+# Create annual_return
+google['annual_return'] = google['Close'].pct_change(periods=360) * 100
+
+# Plot the result
+google.plot(subplots=True)
+plt.show()
+
+# Import data here
+prices = pd.read_csv('asset_classes.csv', parse_dates=['DATE'], index_col=['DATE'])
+
+# Inspect prices here
+print(prices.info())
+
+# Select first prices
+first_prices = prices.iloc[0]
+
+# Create normalized
+normalized = prices.div(first_prices) * 100
+
+# Plot normalized
+normalized.plot()
+plt.show()
+
+# Import stock prices and index here
+stocks = pd.read_csv('nyse.csv', parse_dates=['date'], index_col='date')
+dow_jones = pd.read_csv('dow_jones.csv', parse_dates=['date'], index_col='date')
+
+# Concatenate data and inspect result here
+data = pd.concat([stocks, dow_jones], axis=1)
+print(data.info())
+
+# Normalize and plot your data here
+data.div(data.iloc[0]).mul(100).plot()
+plt.show()
+
+# Create tickers
+tickers = ['MSFT', 'AAPL']
+
+# Import stock data here
+stocks = pd.read_csv('msft_aapl.csv', parse_dates=['date'], index_col='date')
+
+# Import index here
+sp500 = pd.read_csv('sp500.csv', parse_dates=['date'], index_col='date')
+
+# Concatenate stocks and index here
+data = pd.concat([stocks, sp500], axis=1).dropna()
+
+# Normalize data
+normalized = data.div(data.iloc[0]).mul(100)
+
+# Subtract the normalized index from the normalized stock prices, and plot the result
+normalized[tickers].sub(normalized['SP500'], axis=0).plot()
+plt.show()
+
+# Set start and end dates
+start = '2016-1-1'
+end = '2016-2-29'
+
+# Create monthly_dates here
+monthly_dates = pd.date_range(start=start, end=end, freq='M')
+
+# Create and print monthly here
+monthly = pd.Series(data=[1, 2], index=monthly_dates)
+print(monthly)
+
+# Create weekly_dates here
+weekly_dates = pd.date_range(start=start, end=end, freq='W')
+
+# Print monthly, reindexed using weekly_dates
+print(monthly.reindex(weekly_dates))
+print(monthly.reindex(weekly_dates, method='bfill'))
+print(monthly.reindex(weekly_dates, method='ffill'))
+
+# Import data here
+data = pd.read_csv('unemployment.csv', parse_dates=['date'], index_col='date')
+
+# Show first five rows of weekly series
+print(data.asfreq('W').head())
+
+# Show first five rows of weekly series with bfill option
+print(data.asfreq('W', method='bfill').head())
+
+# Create weekly series with ffill option and show first five rows
+weekly_ffill = data.asfreq('W', method='ffill')
+print(weekly_ffill.head())
+
+# Plot weekly_fill starting 2015 here
+weekly_ffill.loc['2015':].plot()
+plt.show()
